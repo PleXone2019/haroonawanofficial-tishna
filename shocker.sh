11933208b7e4dcdc8c6c64a967b842b1c1f0cfd9
Haroon Awan <39236904+haroonawanofficial@users.noreply.github.com>
2019-11-16 23:29:23 +0500

    Update shocker.sh

--- shocker.sh
+++ shocker.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-echo -ne "[+] Enter Target:" 
-read -p Target
+echo -e "Default would be target/cgi-bin/status"
+echo -e "Custom is also possible"
+echo -ne "[+] Enter Target: " 
+read Target
 ./shocker.py $Target
