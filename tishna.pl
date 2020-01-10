78455eb159417b1b66b72cc0cad3853782c171c2
Haroon Awan <mrharoonawan@Gmail.com>
2020-01-10 10:50:57 -0500

    Update tishna.pl

--- tishna.pl
+++ tishna.pl
@@ -1112,7 +1112,7 @@ sub menu {
         enter();
     }if($number eq '48'){
         banner();
-        print line_u(),color('bold cyan'),"        #";print color('reset'),item(),"Enter Target Website in Absolute Format";print color('bold cyan'),"           #   \n",line_d();
+        print line_u(),color('bold cyan'),"        #";print color('reset'),item(),"Enter Target Website without Absolute Format";print color('bold cyan'),"           #   \n",line_d();
         print color('bold green'),"\n\ntishna: _>  ";
         print color('bold white');
         chomp($simplersa=<STDIN>);
@@ -3251,7 +3251,7 @@ my $ua = LWP::UserAgent->new;
 $ua->agent("Mozilla/5.0 (Windows NT 6.1; WOW64; rv:31.0) Gecko/20130401 Firefox/31.0.");
 print color("BOLD WHITE"), "\n[ + ] Checking HTTP Response Splitting [ + ]  \n";
 print color("reset");
-my $req = HTTP::Request->new( GET => "http://$url%0d%0a%0d%0a\";alert('XSS');//" );
+my $req = HTTP::Request->new( GET => "http://$simplersa/%0d%0a%0d%0a\";alert('XSS');//" );
 my $res = $ua->request($req);
 print $res->as_string (); 
 print $req->as_string (); 
